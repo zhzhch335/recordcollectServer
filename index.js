ebf130ded1b4e234b086d62d1487a2ce69550731
zhzhch335 <357611628@qq.com>
2022-02-02 17:43:35 +0800

    跨域问题处理

--- index.js
+++ index.js
@@ -3,18 +3,15 @@ const express = require("express");
 const pathLib = require("path");
 // 创建 application/x-www-form-urlencoded 编码解析
 var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({
-    extended: false
-})
 // 创建文件解析
 const multer = require('multer')
 
 const app = express();
 
-// app.use('/public', express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(bodyParser.json());
 const avatorMulter = multer({
     dest: './avator/'
 }).any();
@@ -23,16 +20,26 @@ const soundMulter = multer({
     dest: "./sound/"
 }).any()
 
+app.use((req, res, next) => {
+    res.set({
+        'Access-Control-Allow-Credentials': true, //允许后端发送cookie
+        'Access-Control-Allow-Origin': '*', //任意域名都可以访问,或者基于我请求头里面的域
+        'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type', //设置请求头格式和类型
+        'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',//允许支持的请求方式
+    })
+    req.method === 'OPTIONS' ? res.status(204).end() : next()
+})
+
 
 app.get('/queryId', function (req, res) {
     fs.readFile(`./database/${req.query.id}.json`, (err, data) => {
+
         if (!err) {
             var queryMeMeRen = JSON.parse(data.toString());
             res.send({
                 data: queryMeMeRen,
                 errorCode: 0,
             });
-            fs.close()
         } else {
             res.send({
                 "errorCode": 1,
@@ -96,10 +103,11 @@ app.post('/uploadAvator', avatorMulter, function (req, res, next) {
 });
 
 app.post('/uploadSound', soundMulter, function (req, res, next) {
-    var newName = req.files[0].path + pathLib.parse(req.files[0].originalname).ext;
+    // var newName = req.files[0].path + pathLib.parse(req.files[0].originalname).ext;
     /**
      * fs 属性 rename 参数1：被改的文件， 参数2：需要更改的名称 ， 参数3：回调函数function(err){}
      */
+    var newName = req.files[0].path + ".mp3";
     fs.rename(req.files[0].path, newName, function (err) {
         if (err)
             res.send({
@@ -111,7 +119,7 @@ app.post('/uploadSound', soundMulter, function (req, res, next) {
                 errorCode: 0,
                 data: {
                     message: "上传成功",
-                    filename: newName.replace(/^avator\\/, "")
+                    filename: newName.replace(/^sound\\/, "")
                 }
             });
     });
@@ -126,7 +134,7 @@ function checkIdVaild(id) {
     }
 }
 
-var server = app.listen(8081, function () {
+var server = app.listen(8080, function () {
 
     var host = server.address().address
     var port = server.address().port
