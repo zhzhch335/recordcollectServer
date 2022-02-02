201c5bb965932311d7bb622969be73954c49c601
zhzhch335 <357611628@qq.com>
2022-02-02 19:34:58 +0800

    加入trycatch避免程序出错卡住

--- index.js
+++ index.js
@@ -33,97 +33,117 @@ app.use((req, res, next) => {
 
 
 app.get('/queryId', function (req, res) {
-    fs.readFile(`./database/${req.query.id}.json`, (err, data) => {
+    try {
+        fs.readFile(`./database/${req.query.id}.json`, (err, data) => {
 
-        if (!err) {
-            var queryMeMeRen = JSON.parse(data.toString());
-            res.send({
-                data: queryMeMeRen,
-                errorCode: 0,
-            });
-        } else {
-            res.send({
-                "errorCode": 1,
-                "errorMessage": "无法查询到此ID，请问琛琛重新要一个网址"
-            })
-        }
-    })
+            if (!err) {
+                var queryMeMeRen = JSON.parse(data.toString());
+                res.send({
+                    data: queryMeMeRen,
+                    errorCode: 0,
+                });
+            } else {
+                res.send({
+                    "errorCode": 1,
+                    "errorMessage": "无法查询到此ID，请问琛琛重新要一个网址"
+                })
+            }
+        })
+    }
+    catch (err) {
+        console.log(err);
+    }
 });
 
 app.post('/addMeMeRen', function (req, res) {
-    var body = req.body;
-    if (body && body.id) {
-        if (checkIdVaild(body.id)) {
-            fs.writeFile(`./database/${body.id}.json`, JSON.stringify(body), err => {
-                if (err) {
-                    res.send({
-                        "errorCode": -1,
-                        "errorMessage": err.message
-                    })
-                } else {
-                    res.send({
-                        errorCode: 0,
-                        errorMessage: "保存成功"
-                    })
-                }
-            })
+    try {
+        var body = req.body;
+        if (body && body.id) {
+            if (checkIdVaild(body.id)) {
+                fs.writeFile(`./database/${body.id}.json`, JSON.stringify(body), err => {
+                    if (err) {
+                        res.send({
+                            "errorCode": -1,
+                            "errorMessage": err.message
+                        })
+                    } else {
+                        res.send({
+                            errorCode: 0,
+                            errorMessage: "保存成功"
+                        })
+                    }
+                })
+            } else {
+                res.send({
+                    "errorCode": 1,
+                    "errorMessage": "不存在该条记录，请确认网址完整性"
+                })
+            }
         } else {
             res.send({
                 "errorCode": 1,
-                "errorMessage": "不存在该条记录，请确认网址完整性"
+                "errorMessage": "id参数缺失"
             })
         }
-    } else {
-        res.send({
-            "errorCode": 1,
-            "errorMessage": "id参数缺失"
-        })
+    }
+    catch (err) {
+        console.log(err);
     }
 });
 
 app.post('/uploadAvator', avatorMulter, function (req, res, next) {
-    var newName = req.files[0].path + pathLib.parse(req.files[0].originalname).ext;
-    /**
-     * fs 属性 rename 参数1：被改的文件， 参数2：需要更改的名称 ， 参数3：回调函数function(err){}
-     */
-    fs.rename(req.files[0].path, newName, function (err) {
-        if (err)
-            res.send({
-                errorCode: 1,
-                errorMessage: "上传失败"
-            });
-        else
-            res.send({
-                errorCode: 0,
-                data: {
-                    message: "上传成功",
-                    filename: newName.replace(/^avator\\/, "")
-                }
-            });
-    });
+    try {
+        var newName = req.files[0].path + pathLib.parse(req.files[0].originalname).ext;
+        /**
+         * fs 属性 rename 参数1：被改的文件， 参数2：需要更改的名称 ， 参数3：回调函数function(err){}
+         */
+        fs.rename(req.files[0].path, newName, function (err) {
+            if (err)
+                res.send({
+                    errorCode: 1,
+                    errorMessage: "上传失败"
+                });
+            else
+                res.send({
+                    errorCode: 0,
+                    data: {
+                        message: "上传成功",
+                        filename: newName.replace(/^avator\\/, "")
+                    }
+                });
+        });
+    }
+    catch (err) {
+        console.log(err);
+    }
 });
 
 app.post('/uploadSound', soundMulter, function (req, res, next) {
-    // var newName = req.files[0].path + pathLib.parse(req.files[0].originalname).ext;
-    /**
-     * fs 属性 rename 参数1：被改的文件， 参数2：需要更改的名称 ， 参数3：回调函数function(err){}
-     */
-    var newName = req.files[0].path + ".mp3";
-    fs.rename(req.files[0].path, newName, function (err) {
-        if (err)
-            res.send({
-                errorCode: 1,
-                errorMessage: "上传失败"
-            });
-        else
-            res.send({
-                errorCode: 0,
-                data: {
-                    message: "上传成功",
-                    filename: newName.replace(/^sound\\/, "")
-                }
-            });
-    });
+    try {
+        // var newName = req.files[0].path + pathLib.parse(req.files[0].originalname).ext;
+        /**
+         * fs 属性 rename 参数1：被改的文件， 参数2：需要更改的名称 ， 参数3：回调函数function(err){}
+         */
+        var newName = req.files[0].path + ".mp3";
+        fs.rename(req.files[0].path, newName, function (err) {
+            if (err)
+                res.send({
+                    errorCode: 1,
+                    errorMessage: "上传失败"
+                });
+            else
+                res.send({
+                    errorCode: 0,
+                    data: {
+                        message: "上传成功",
+                        filename: newName.replace(/^sound\\/, "")
+                    }
+                });
+        });
+    }
+    catch (err) {
+        console.log(err)
+    }
 })
 
 function checkIdVaild(id) {
@@ -135,7 +155,7 @@ function checkIdVaild(id) {
     }
 }
 
-var server = app.listen(8080, function () {
+var server = app.listen(80, function () {
 
     var host = server.address().address
     var port = server.address().port
